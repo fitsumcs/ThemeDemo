f2714c26dc52f9d5af2a1e7d9b8778012c4d99b1
Fitsum Alemu <fitsumcs@gmail.com>
2021-04-15 19:59:03 +0400

    update the side bar

--- Version II/functions.php
+++ Version II/functions.php
@@ -26,6 +26,10 @@ function my_custom_theme_sidebar() {
   register_sidebar( array(
       'name' => __( 'Primary Sidebar', 'my-custom-theme' ),
       'id'   => 'sidebar-1',
+       'before_widget' => '<div>',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h2 class="rounded">',
+      'after_title'   => '</h2>',
   ) );
 }
 add_action( 'widgets_init', 'my_custom_theme_sidebar' );
